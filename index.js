3544198afd9a136dfc7593ff8999f7741b0254fd
HabibJR <smhabib.abir2030@gmail.com>
2022-10-10 09:53:49 +0200

    add modal box to sh

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-$(document).ready(function(){
-    $("#submit-iteam").click(function(e){
-        e.preventDefault();
-        var new_iteam = $("#new-iteam").val();
-        
-        if(new_iteam != ""){
-            var newIteam = $("#iteam").clone().removeClass("d-none").addClass("new-iteam");
-            newIteam.find(".iteams").innerText = new_iteam;
-            $("#iteam-list").append(newIteam);
-        }else{
-            alert("Iteam can not be empty!");
-        }
+$(document).ready(function () {
 
+  $("#submit-iteam").click(function (e) {
+    e.preventDefault();
+    var new_iteam = $("#new-iteam").val();
 
-    })
-})
+    if (new_iteam != "") {
+      var newIteam = $(".new-added-iteam")
+        .clone()
+        .removeClass("new-added-iteam d-none")
+        .addClass("new-iteam");
+      newIteam.find(".itam-name").addClass("new_added_iteam_name");
+      newIteam.find(".new_added_iteam_name").text(new_iteam);
+      $("#iteam-list").append(newIteam);
+    } else {
+      $("#myModalBox").modal("show");
+    }
+  });
+});
