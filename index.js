3f2283a3f6098e82061f9fa05113955cb9345066
HabibJR <smhabib.abir2030@gmail.com>
2022-10-10 10:29:20 +0200

    add delete and modal box

--- index.js
+++ index.js
@@ -12,8 +12,31 @@ $(document).ready(function () {
       newIteam.find(".itam-name").addClass("new_added_iteam_name");
       newIteam.find(".new_added_iteam_name").text(new_iteam);
       $("#iteam-list").append(newIteam);
+
+      $(".to-do-done").on("change", function () {
+        // on change of state
+        if (this.checked) {
+          // if changed state is "CHECKED"
+          // delete the iteam form list
+          var parent_row = $(this).closest(".new-iteam");
+          parent_row.find("#asdf").wrap("<strike>");
+          parent_row.fadeOut("slow");
+        }
+      });
+
+      $("#new-iteam").val('');
+
     } else {
       $("#myModalBox").modal("show");
     }
   });
+
+
+ 
+
+
+  
+
+
+
 });
